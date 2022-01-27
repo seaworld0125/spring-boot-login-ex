371b8a3a1a973152322b7284173c5edefbecae48
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-01-27 23:00:35 +0900

    study

--- src/main/java/org/example/LoginServerApplication.java
+++ src/main/java/org/example/LoginServerApplication.java
@@ -2,9 +2,6 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LoginServerApplication {
--- src/main/java/org/example/domain/account/controller/SignController.java
+++ src/main/java/org/example/domain/account/controller/SignController.java
@@ -1,12 +1,10 @@
 package org.example.domain.account.controller;
 
-import java.util.HashMap;
 import lombok.RequiredArgsConstructor;
 import org.example.domain.member.repository.MemberRepository;
 import org.example.domain.member.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
